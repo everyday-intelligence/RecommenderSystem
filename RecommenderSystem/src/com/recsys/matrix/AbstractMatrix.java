10e6b0a337111ad2e106236d016ef8e49176221c
everyday-intelligence <everyday.intelligence@gmail.com>
2012-07-01 16:43:23 +0100

    Signed-off-by: everyday-intelligence <everyday.intelligence@gmail.com>

--- RecommenderSystem/src/com/recsys/matrix/AbstractMatrix.java
+++ RecommenderSystem/src/com/recsys/matrix/AbstractMatrix.java
@@ -2,6 +2,8 @@ package com.recsys.matrix;
 
 public abstract class AbstractMatrix {
 
+	protected abstract int getColumnsNumber();
+	protected abstract int getRowsNumber();
 	protected abstract Double get(int i, int j);
 	protected abstract boolean set(int i, int j, Double vals);
 
